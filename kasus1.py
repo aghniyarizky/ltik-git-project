d3acaf444cda15177db241d7d32d5d70b7ef5832
aghniyarizky <aghniyarizky@gmail.com>
2025-11-05 12:29:52 +0700

    modifikasi di kasus1.py

--- kasus1.py
+++ kasus1.py
@@ -4,6 +4,5 @@ import numpy as np
 suhu = np.array([20, 15, 21, 22, 18, 19, 20, 17, 16, 15])
 konversi = (suhu * 9/5) + 32
 tipedata = np.array(konversi, dtype="i")
-print(suhu, "= derajat Celcius")
-# print(konversi, "= derajat Fahrenheit")
-print(tipedata, "= derajat Fahrenheit")
+print("Derajat Celcius", suhu)
+print("Derajat Fahrenheit", tipedata)
